582c29eeb4bc66aac1acc3b8772feca88ac3d404
Vadim <lyzhinv@gmail.com>
2023-01-31 20:44:23 +0300

    Add Main block for Telegram bot v2

---
+++ main_v2.py
@@ -0,0 +1,16 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from bot_commands_v2 import *
+
+app = ApplicationBuilder().token("5994530779:AAF1ISLKR1llPXf54B8HLGTSx-kn1u51td4").build()
+
+app.add_handler(CommandHandler("start", start_command))
+app.add_handler(CommandHandler("hello", hi_command))
+app.add_handler(CommandHandler("time", time_command))
+app.add_handler(CommandHandler("date", date_command))
+app.add_handler(CommandHandler("ny", ny_command))
+app.add_handler(CommandHandler("sum", sum_command))
+app.add_handler(CommandHandler("english", eng_learn_command))
+
+print('Bot start')
+app.run_polling()
